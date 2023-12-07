b5e54dc47bc5e51baaacbfdb5b79de8e3362b8af
umari2786786 <oiqbal2786@gmail.com>
2023-12-07 01:12:33 +0000

    test first commit from intelli

---
+++ src/main/java/Class13/Maths.java
@@ -0,0 +1,8 @@
+package Class13;
+
+public class Maths {
+    void add (int input1, int input2){
+        System.out.println(input1+input2);
+
+    }
+}
---
+++ src/main/java/Class13/MathsTester.java
@@ -0,0 +1,8 @@
+package Class13;
+
+public class MathsTester {
+    public static void main(String[] args) {
+        Maths obj= new Maths();
+        obj.add(20,10);
+    }
+}

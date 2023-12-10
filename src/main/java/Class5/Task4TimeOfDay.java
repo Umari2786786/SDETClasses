b298e942d5bf301f87188ecd5a7d6270df6590c9
umari2786786 <oiqbal2786@gmail.com>
2023-12-10 00:10:37 +0000

    test second commit from intelli

---
+++ src/main/java/Class5/Task4TimeOfDay.java
@@ -0,0 +1,23 @@
+package Class5;
+
+import java.util.Scanner;
+
+public class Task4TimeOfDay {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the time using 24 hour clock format eg 00.00");
+        double time= sc.nextFloat();
+        if (time>=00.00&&time<=11.59){
+            System.out.println("Its morning here");
+        }else if (time>=12.00&&time<=15.59){
+            System.out.println("Its the afternoon here");
+        }else if (time>=16.00&&time<=20.59){
+            System.out.println("Its the evening here");
+        }else if (time>=21.00 || (time>=00.00 && time <=23.59)){
+            System.out.println("Its night time here");
+        }else {
+            System.out.println("Sorry an invalid time entered");
+            sc.close();
+        }
+    }
+}
